e47807867c9cec4c7f2821512918c0fcea1c092b
Uan <uanesfrancisco@gmail.com>
2025-07-14 11:53:32 -0300

    first commit

---
+++ src/main/java/dev/uanes/cadastroDeNinja/ApiCadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.uanes.cadastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroDeNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uanes/cadastroDeNinja/ApiCadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uanes.cadastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
