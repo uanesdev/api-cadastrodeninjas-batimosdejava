6c360af8fe081971f592b1912d0f62462d9fcf6e
Uan <uanesfrancisco@gmail.com>
2025-07-15 15:32:17 -0300

    Adiconando depenência do Lombok e configurando o banco de dados, relacionando as tabelas de NinjaModel e MissaoModel

--- src/main/java/dev/uanes/cadastroDeNinja/ninjas/Controller.java
+++ src/main/java/dev/uanes/cadastroDeNinja/ninjas/Controller.java
@@ -1,4 +1,4 @@
-package dev.uanes.cadastroDeNinja.controller;
+package dev.uanes.cadastroDeNinja.ninjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/dev/uanes/cadastroDeNinja/ninjas/NinjaModel.java
+++ src/main/java/dev/uanes/cadastroDeNinja/ninjas/NinjaModel.java
@@ -1,9 +1,16 @@
 package dev.uanes.cadastroDeNinja.ninjas;
 
+import dev.uanes.cadastroDeNinja.missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -13,42 +20,9 @@ public class NinjaModel {
     private String email;
     private int idade;
 
-    // NoArgsConstructor
-    public NinjaModel() {
-    }
+    // @ManyToOne - Um Ninja tem uma única missão!
+    @ManyToOne
+    @JoinColumn(name = "missao_id") // Foreing Key - Chave estrangeira, uma tabela dentro de uma tabela
+    private MissaoModel missao;
 
-    // AllArgsConstructor
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
