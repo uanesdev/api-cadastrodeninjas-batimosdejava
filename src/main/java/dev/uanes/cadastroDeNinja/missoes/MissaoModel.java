ff3a4b7488c67feb13e1bcf3aa3dbf5a4f7daf35
Uan <uanesfrancisco@gmail.com>
2025-07-15 15:51:03 -0300

    feat: Adiciona a entidade MissaoModel, já relacionada com a entidade NinjaModel

---
+++ src/main/java/dev/uanes/cadastroDeNinja/missoes/MissaoModel.java
@@ -0,0 +1,28 @@
+package dev.uanes.cadastroDeNinja.missoes;
+
+import dev.uanes.cadastroDeNinja.ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private char rank;
+
+    // @OneToMany - Uma missão pode ter vários ninjas
+    @OneToMany(mappedBy = "missao")
+    private List<NinjaModel> ninjas;
+
+}
