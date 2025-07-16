67e65f378851b3b77012515e4a2ef1bcc53ef641
Uan <uanesfrancisco@gmail.com>
2025-07-15 22:17:19 -0300

    feat: Criando a MissaoRepository extendendo o JPA.

---
+++ src/main/java/dev/uanes/cadastroDeNinja/missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package dev.uanes.cadastroDeNinja.missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
