bd1c383744b5f7675ebe67dcfab5dc2574ceba78
Uan <uanesfrancisco@gmail.com>
2025-07-17 21:10:35 -0300

    feat: Cria o NinjaRepository

---
+++ src/main/java/dev/uanes/cadastroDeNinja/ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.uanes.cadastroDeNinja.ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
